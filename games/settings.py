b34586cf9556f041b88faeb41dd4ad205bb5bbcd
kfullerton75 <kfullerton752@gmail.com>
2023-10-11 15:01:23 -0700

    move build folder

--- games/settings.py
+++ games/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j3zud$^ywg7m7j8@wirerv2x*8)nh&wsp#ens2k3bbw-x61wr!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["3.135.120.157", "0.0.0.0", "127.0.0.1", "localhost"]
 
